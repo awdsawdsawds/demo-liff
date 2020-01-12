8446c845cbb8eded5e1d635a73bfdcaced276ad2
Vattanan Buranapugdee <vattanan.bu@gmail.com>
2020-01-12 16:52:57 +0700

    add demo liff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [profile, setProfile] = useState({})
+  window.liff.init()
+    .then(() => {
+      // TODO: implement this
+      console.log('success')
+    })
+    .catch((error) => {
+      // TODO: implement this
+      console.error(error)
+    })
+
+    const getProfileFromLine = async () => {
+      const dataInfo = await window.liff.getProfile()
+      setProfile(dataInfo)
+    }
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Profile: { JSON.stringify(profile) }
         </p>
-        <a
+        <button
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          onClick={getProfileFromLine}
         >
-          Learn React
-        </a>
+          Get Profile
+        </button>
       </header>
     </div>
   );
