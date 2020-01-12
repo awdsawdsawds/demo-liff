1f8845d0eb1c78b6eb531463562efafcb514d8f6
Vattanan Buranapugdee <vattanan.bu@gmail.com>
2020-01-12 17:21:04 +0700

    Updates

---
+++ precache-manifest.1bfeb8c1585e0744c7bd64db5ff7fe5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "460be3c485ce3c252bfad7ffebd9e039",
+    "url": "/demo-liff/index.html"
+  },
+  {
+    "revision": "263eab88d26ac9e5aca3",
+    "url": "/demo-liff/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "80cc11c691b16af8af36",
+    "url": "/demo-liff/static/js/2.f74d40cd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/demo-liff/static/js/2.f74d40cd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "263eab88d26ac9e5aca3",
+    "url": "/demo-liff/static/js/main.260f14f1.chunk.js"
+  },
+  {
+    "revision": "54be687b9e83b0e3f1bc",
+    "url": "/demo-liff/static/js/runtime-main.c5e30651.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-liff/static/media/logo.5d5d9eef.svg"
+  }
+]);
